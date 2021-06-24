80ecfedf1a8f0175ace7e0e1deb504727c695c74
Audrey <aerv@ic.ufal.br>
2021-06-24 10:37:17 -0300

    Adicionando infos sobre empregados

--- sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/Empregados.java
+++ sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/Empregados.java
@@ -39,4 +39,11 @@ public class Empregados {
 		this.definirEndereco(endereco);
 		this.metodoPagamento = metodo;
 	}
+	
+	public void infoEmpregados() {
+		System.out.println("ID: " + this.pegarId());
+		System.out.println("Nome: " + this.pegarNome());
+		System.out.println("Endereço: " + pegarEndereco());
+		System.out.println("Tipo de empregado: " + pegarMetodoPagamento());
+	}
 }
