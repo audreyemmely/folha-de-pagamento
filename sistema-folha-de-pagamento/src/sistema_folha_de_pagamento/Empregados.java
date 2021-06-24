ac0129fe2f6449169c1759c45b1ba248d71a3030
Audrey <aerv@ic.ufal.br>
2021-06-24 15:38:14 -0300

    Estruturando melhor a classe

--- sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/Empregados.java
+++ sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/Empregados.java
@@ -1,49 +1,101 @@
 package sistema_folha_de_pagamento;
 
 import java.lang.String;
+import java.util.Scanner;
 
 public class Empregados {
-	
-	private int id;
+	//organizar melhor esses dados depois
+	private int idEmpregado = 0;
 	private String nome;
 	private String endereco;
+	private String tipoEmpregado;
 	private String metodoPagamento;
+	private String tipoAgenda;
+	private int dataPagamento;
+	private int diaSemanaPagamento;
+	protected double salarioMensal;
+	protected double salarioLiquido;
+	protected boolean empregadoSindicalista = false;
+	private int idSindicato = -1;
+	private double taxaSindicato = 0;
+	private double outrasTaxas = 0;
+	
+	//Infos principais do empregado
+	public void definirNome() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digite o nome completo do empregado:");
+		this.nome = input.nextLine();
+		input.close();
+	}
 	
 	public String pegarNome() {
 		return nome;
 	}
+
 	
-	public void definirNome(String novoNome) {
-		this.nome = novoNome;
+	public void definirEndereco() {
+		Scanner input = new Scanner(System.in);
+		this.endereco = input.nextLine();
+		input.close();
 	}
 	
 	public String pegarEndereco() {
 		return endereco;
 	}
 	
-	public void definirEndereco(String novoEndereco) {
-		this.endereco = novoEndereco;
+
+	
+	public boolean pegarEmpregadoSindicalista() {
+		return empregadoSindicalista;
+	}
+	
+	//Pagamento
+	public void definirMetodoPagamento(String metodoPagamentoEmp) {
+		this.metodoPagamento = metodoPagamentoEmp;
 	}
 	
 	public String pegarMetodoPagamento() {
 		return metodoPagamento;
 	}
 	
-	public int pegarId() {
-		return id;
+	//Id empregado
+	public void definirIdEmpregado(int idEmpregado) {
+		this.idEmpregado = idEmpregado;
+	}
+	
+	public int pegarIdEmpregado() {
+		return this.idEmpregado;
 	} 
 	
-	public Empregados(int id, String nome, String endereco, String metodo) {
-		this.id = id;
-		this.definirNome(nome);
-		this.definirEndereco(endereco);
-		this.metodoPagamento = metodo;
+	//Tipo de empregado
+	public void definirTipoEmpregado(String tipoEmp) {
+		this.tipoEmpregado = tipoEmp;
 	}
 	
-	public void infoEmpregados() {
-		System.out.println("ID: " + this.pegarId());
-		System.out.println("Nome: " + this.pegarNome());
-		System.out.println("Endereço: " + pegarEndereco());
-		System.out.println("Tipo de empregado: " + pegarMetodoPagamento());
+	public String pegarTipoEmpregado() {
+		return tipoEmpregado;
 	}
+	
+	//Tipo de agenda
+	public void definirTipoAgenda(String tipoAgendaEmp) {
+		this.tipoAgenda = tipoAgendaEmp;
+	}
+	
+	public String pegarTipoAgenda() {
+		return tipoAgenda;
+	}
+	
+	//Data pagamento
+	public void definirDataPagamento(int dataPagEmp) {
+		this.dataPagamento = dataPagEmp;
+	}
+	
+	public int pegarDataPagamento() {
+		return dataPagamento;
+	}
+	
+	//Dia da semana do pagamento
+	
+	//cansei, depois termino
+
 }
