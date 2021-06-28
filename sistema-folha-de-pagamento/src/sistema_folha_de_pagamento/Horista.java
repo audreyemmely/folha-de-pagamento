cad85f6232987958f09f480d09a6913b8859fed0
Audrey <aerv@ic.ufal.br>
2021-06-28 19:48:38 -0300

    Criando classe horista

---
+++ sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/Horista.java
@@ -0,0 +1,11 @@
+package sistema_folha_de_pagamento;
+
+public class Horista extends Empregados {
+
+	@Override
+	public double definirSalarioLiquidoEmpregado() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
