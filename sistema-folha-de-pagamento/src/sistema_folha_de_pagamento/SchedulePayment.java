c2fd44942ef98051ef1051acbb233e37923c5a1a
Audrey <aerv@ic.ufal.br>
2021-08-11 23:11:16 -0300

    Update SchedulePayment.java

--- sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/SchedulePayment.java
+++ sistema-folha-de-pagamento/src/sistema_folha_de_pagamento/SchedulePayment.java
@@ -8,9 +8,9 @@ public class SchedulePayment {
 		for (int i = 0; i < employees.size(); ++i) {
 			if(employees.get(i).getScheduleType().equals("SEMANAL") && employees.get(i).getPaymentWeeklyDay() == weeklyDay) {
 				System.out.println("ID: " + employees.get(i).getEmployeeId() +
-									"\nNome: " + employees.get(i).getName() +
-									"\nSalário: R$ " + ((Hourly )employees.get(i)).setEmployeeLiquidSalary() +
-									"\nMétodo de pagamento: " + employees.get(i).getPaymentMethod() + "\n");
+						"\nNome: " + employees.get(i).getName() +
+						"\nSalário: R$ " + ((Hourly )employees.get(i)).setEmployeeLiquidSalary() +
+						"\nMétodo de pagamento: " + employees.get(i).getPaymentMethod() + "\n");
 				((Hourly )employees.get(i)).setDaySalary();
 				employees.get(i).setEmployeeLiquidSalary(0);
 			}
@@ -21,8 +21,8 @@ public class SchedulePayment {
 		for(int i = 0; i < employees.size(); ++i) {
 			if(employees.get(i).getScheduleType().equals("BI-SEMANAL") && employees.get(i).getPaymentWeeklyDay() == weeklyDay) {
 				System.out.println("ID: " + employees.get(i).getEmployeeId() + "\nNome: " + employees.get(i).getName() +
-									"\nSalário: R$ " + ((Commissioned )employees.get(i)).setEmployeeLiquidSalary() +
-									"\nMétodo de pagamento: " + employees.get(i).getPaymentMethod() + "\n");
+							"\nSalário: R$ " + ((Commissioned )employees.get(i)).setEmployeeLiquidSalary() +
+							"\nMétodo de pagamento: " + employees.get(i).getPaymentMethod() + "\n");
 				employees.get(i).setEmployeeLiquidSalary(0);
 			}
 		}
@@ -32,8 +32,8 @@ public class SchedulePayment {
 		for(int i = 0; i < employees.size(); ++i) {
 			if(employees.get(i).getScheduleType().equals("MENSAL") && employees.get(i).getPaymentDate() == paymentDate) {
                 System.out.println("ID: " + employees.get(i).getEmployeeId() + "\nNome: " + employees.get(i).getName() +
-                					"\nSalário: R$ " +((Salaried )employees.get(i)).setEmployeeLiquidSalary() + 
-                					"\nMétodo de pagamento: " + employees.get(i).getPaymentMethod() + "\n");
+                		"\nSalário: R$ " +((Salaried )employees.get(i)).setEmployeeLiquidSalary() + 
+                		"\nMétodo de pagamento: " + employees.get(i).getPaymentMethod() + "\n");
                 employees.get(i).setEmployeeLiquidSalary(0);
 			}
 		}
